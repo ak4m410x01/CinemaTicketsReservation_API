fe211c6fffa64e75fa55ab0e5555d640d62fd367
ak4m410x01 <abdallah10kamal@gmail.com>
2023-11-24 00:35:05 +0200

    create post model

--- tickets/models.py
+++ tickets/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
 # Create your models here.
@@ -35,6 +36,12 @@ class Reservation(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie}:{self.guest}"
+
+
+class post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    body = models.TextField(max_length=50_000)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
